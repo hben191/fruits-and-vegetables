799c50670a1509426bf34b4dd92a2f973ca8ef8f
Hamza <benbrahim.hamza@proton.me>
2025-02-07 14:49:37 +0100

    created abstract food entity with vegetable and fruit subclasses

---
+++ src/Entity/Fruit.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FruitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FruitRepository::class)]
+class Fruit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
