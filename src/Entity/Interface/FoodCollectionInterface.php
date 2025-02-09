04aca5ec26a724a0f94de683f2656efe1e202f01
Hamza <benbrahim.hamza@proton.me>
2025-02-09 20:07:13 +0100

    added some phpdoc for clarity

--- src/Entity/Interface/FoodCollectionInterface.php
+++ src/Entity/Interface/FoodCollectionInterface.php
@@ -6,14 +6,25 @@ use App\Entity\AbstractFood;
 
 interface FoodCollectionInterface
 {
+    /**
+     * Add an AbstractFood object
+     * @param AbstractFood $food Item to be added to the collection
+     *
+     * @return void
+     */
     public function add(AbstractFood $food): void;
 
     /**
+     * Remove an AbstractFood object
+     * @param AbstractFood $food Item to be removed from the collection
+     *
      * @return bool Return true if the object has been successfully removed, returns false if not
      */
-    public function remove(AbstractFood $food): void;
+    public function remove(AbstractFood $food): bool;
 
     /**
+     * List all AbstractFoods in the collection
+     *
      * @return array<int, AbstractFood>
      */
     public function list(): array;
